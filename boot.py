c28094e8db00dd2004143aa49fba9fd286360092
Christian Kraemer <christian.kraemer@tngtech.com>
2017-02-23 23:08:55 +0100

    wlan connection

--- boot.py
+++ boot.py
@@ -1,5 +1,21 @@
 # boot.py -- run on boot-up
 import os
-from machine import UART
+from network import WLAN
+from machine import UART,  idle
+import config
+
 uart = UART(0, 115200)
 os.dupterm(uart)
+
+wlan = WLAN(mode=WLAN.STA)
+
+if not wlan.isconnected():
+    print("Connecting ...")
+    wlan.connect(config.SSID, auth=(WLAN.WPA2, config.PASSWORD), timeout=5000)
+    print("Waiting for wifi network:{}".format(config.SSID))
+    while not wlan.isconnected():
+        idle() # save power while waiting
+    
+    print("Connected to network:{}".format(config.SSID))
+
+print(wlan.ifconfig())
