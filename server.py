d276e8945f14499c83a72d376dea66b1ad14323c
Christian Kraemer <christian.kraemer@tngtech.com>
2017-02-24 15:47:39 +0100

    pwm version

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import machine
 import socket
 import re
 
+PINS=["P" + str(i) for i in range(0, 13)]
+
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
@@ -18,28 +20,45 @@ def parse_request(req):
     if m:
         return (m.group(1), m.group(2))
     else:
-        print("Could not parse: {}".format(url))
+        print("Could not parse: {}".format(req))
         return ("", "")
 
 def parse_url(url):
-    m = re.match("/(P[0-9][0-9])/[0-9]+", url)
+    m = re.match("/(P[0-9]+)/([0-9]+)", url)
     if m:
         return (m.group(1), m.group(2))
     else:
         print("Could not parse: {}".format(url))
         return ("", "")
 
+def process_header(c):
+    while True:
+        header = c.readline()
+        if header == b"" or header == b"\r\n":
+            break
+        print("H:{}".format(header))
+
+
 while True:
     c, addr = s.accept()
     print('client connected from', addr)
     req = c.readline()
+    process_header(c)
+
     (method, url) = parse_request(req)
     print("req: {} url: {}".format(method, url))
 
     if method == 'GET':
         (pin, level) = parse_url(url)
-        print("pin: {} level: {}".format(pin, level)
-        pwm_c = pwm.channel(PWM_CHANNEL, pin='P12', duty_cycle=0.5)
+        if pin in PINS:
+            pwm_c = pwm.channel(PWM_CHANNEL, pin=pin, duty_cycle=float(level)/100)
+            print("Pin {} set to level {}".format(pin, level))
+            c.send("HTTP/1.0 200 OK\n\nPin {} set to level {}\n\n".format(pin, level))
+        else:
+            c.send("HTTP/1.0 200 OK\n\nPin \"{}\" unknown\n".format(pin, level))
+            
 
-    c.send("HTTP/1.0 200 OK\n\nOK!\n")
+    else:
+        c.send("HTTP/1.0 200 OK\n\nNo pin set\n")
+
     c.close()
