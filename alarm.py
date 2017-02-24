e68b1875adbaf3cbdec63ace19b35d11b2316207
Christian Kraemer <christian.kraemer@tngtech.com>
2017-02-24 23:52:56 +0100

    alarm

---
+++ alarm.py
@@ -0,0 +1,51 @@
+from machine import Timer
+import time
+
+FREQ = 1.0
+
+class AlarmHandler:
+    i=0
+    duty_cycle = {}
+    enable = True
+
+    @classmethod
+    def pwm(self, i, duty_cycle):
+        duty_cycle = self.duty_cycle
+
+        for pin in duty_cycle:
+            if duty_cycle[pin] == 0 or duty_cycle[pin] == 100:
+                on = duty_cycle[pin] == 100
+            else:
+                on = False
+                if duty_cycle[pin] < 50:
+                    on = i % (100/duty_cycle[pin]) == 0
+                else:
+                    on = i % (100/(100-duty_cycle[pin])) == 0
+            print("Pin: {} is {}".format(pin, on))
+
+    @classmethod
+    def handler(self, alarm):
+        if self.enable == False:
+            print("alarm disabled")
+            alarm.__del__()
+            return
+
+        self.i=self.i+1
+        self.i=self.i % 100
+
+        self.pwm(i)
+
+    @classmethod
+    def disable(self):
+        self.enable = False
+
+    @classmethod
+    def enable(self):
+        self.enable = True
+
+    @classmethod
+    def set_duty_cycle(self, pin, value):
+        self.duty_cycle[pin] = value
+
+
+alarm = Timer.Alarm(AlarmHandler.handler, us=1/FREQ*1e6, periodic=True)
