615026526f89afadba10b1b1d0e46ac498271d04
Christian Kraemer <christian.kraemer@tngtech.com>
2017-02-25 01:00:13 +0100

    alarm

--- alarm.py
+++ alarm.py
@@ -1,33 +1,34 @@
 from machine import Timer
 import time
+import machine
 
-INTERVAL = 1000000
+INTERVAL = 50000
 
 class AlarmHandler:
     i=0
     duty_cycle = {}
+    p_out = {}
     enable = True
 
     @classmethod
-    def pwm(self, i, duty_cycle):
+    def pwm(self, i):
         duty_cycle = self.duty_cycle
 
         for pin in duty_cycle:
+            on = False
             if duty_cycle[pin] == 0 or duty_cycle[pin] == 100:
                 on = duty_cycle[pin] == 100
             else:
-                on = False
                 if duty_cycle[pin] < 50:
-                    on = i % (100/duty_cycle[pin]) == 0
+                    on = i % (100//duty_cycle[pin]) == 0
                 else:
-                    on = i % (100/(100-duty_cycle[pin])) == 0
-            print("Pin: {} is {}".format(pin, on))
+                    on = i % (100//(100-duty_cycle[pin])) != 0
+            #print("Pin: {} is {} for i={}".format(pin, on, i))
+            self.p_out[pin](on)
 
     @classmethod
     def handler(self, alarm):
         if self.enable == False:
-            print("alarm disabled")
-            alarm.__del__()
             return
 
         self.i=self.i+1
@@ -38,14 +39,18 @@ class AlarmHandler:
     @classmethod
     def disable(self):
         self.enable = False
+        print("alarm disabled")
 
     @classmethod
     def enable(self):
         self.enable = True
+        print("alarm enabled")
 
     @classmethod
     def set_duty_cycle(self, pin, value):
         self.duty_cycle[pin] = value
+        self.p_out[pin] = machine.Pin(pin, mode=machine.Pin.OUT)  
 
 
 alarm = Timer.Alarm(AlarmHandler.handler, us=INTERVAL, periodic=True)
+AlarmHandler.set_duty_cycle('P9', 99)
